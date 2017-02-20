12ca0f72d47d4e1154857dffb8e405845815f174
Jungil Han <jungil.han@gmail.com>
2017-02-20 22:57:54 +0800

    #7 시간 정보 단수, 복수 처리

--- api/src/main/java/org/petabytes/api/util/Dates.java
+++ api/src/main/java/org/petabytes/api/util/Dates.java
@@ -11,13 +11,17 @@ public class Dates {
     public static String getRelativeTimeString(long millis) {
         long now = System.currentTimeMillis();
         if (now - millis < TimeUnit.MINUTES.toMillis(1)) {
-            return TimeUnit.MILLISECONDS.toSeconds(now - millis) + " seconds ago";
+            long seconds = TimeUnit.MILLISECONDS.toSeconds(now - millis);
+            return seconds == 1 ? seconds + " second ago" : seconds + " seconds ago";
         } else if (now - millis < TimeUnit.HOURS.toMillis(1)) {
-            return TimeUnit.MILLISECONDS.toMinutes(now - millis) + " minutes ago";
+            long minutes = TimeUnit.MILLISECONDS.toMinutes(now - millis);
+            return minutes == 1 ? minutes + " minute ago" : minutes + " minutes ago";
         } else if (now - millis < TimeUnit.DAYS.toMillis(1)) {
-            return TimeUnit.MILLISECONDS.toHours(now - millis) + " hours ago";
+            long hours = TimeUnit.MILLISECONDS.toHours(now - millis);
+            return hours == 1 ? hours + " hour ago" : hours + " hours ago";
         } else {
-            return TimeUnit.MILLISECONDS.toDays(now - millis) + " days ago";
+            long days = TimeUnit.MILLISECONDS.toDays(now - millis);
+            return days == 1 ? days + " day ago" : days + " days ago";
         }
     }
 
--- api/src/test/java/org/petabytes/api/util/DatesTest.java
+++ api/src/test/java/org/petabytes/api/util/DatesTest.java
@@ -15,10 +15,15 @@ public class DatesTest {
 
     @Test
     public void getRelativeTimeString() throws Exception {
+        Assert.assertEquals("1 second ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(1)));
+        Assert.assertEquals("1 minute ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(1)));
+        Assert.assertEquals("1 hour ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.HOURS.toMillis(1)));
+        Assert.assertEquals("1 day ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(1)));
+
         Assert.assertEquals("30 seconds ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(30)));
         Assert.assertEquals("20 minutes ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.MINUTES.toMillis(20)));
         Assert.assertEquals("4 hours ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.HOURS.toMillis(4)));
-        Assert.assertEquals("1 days ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(1)));
+        Assert.assertEquals("2 days ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(2)));
         Assert.assertEquals("35 days ago", Dates.getRelativeTimeString(System.currentTimeMillis() - TimeUnit.DAYS.toMillis(35)));
     }
 
