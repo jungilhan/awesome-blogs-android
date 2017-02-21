9630fb7ff0edc6c749a04a70cb6c24310d886345
Jungil Han <jungil.han@gmail.com>
2017-02-21 20:21:17 +0800

    DatesTest$getDefaultDateFormats() 함수 Fail되지 않도록 테스트 코드 수정

--- api/src/test/java/org/petabytes/api/util/DatesTest.java
+++ api/src/test/java/org/petabytes/api/util/DatesTest.java
@@ -2,6 +2,7 @@ package org.petabytes.api.util;
 
 import junit.framework.Assert;
 
+import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.junit.Test;
 
@@ -30,7 +31,7 @@ public class DatesTest {
     @Test
     public void getDefaultDateFormats() throws ParseException {
         Assert.assertEquals("30 seconds ago", Dates.getRelativeTimeString(DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZZ")
-            .parseDateTime("2017-02-18T13:40:00.000+09:00")
+            .parseDateTime(new DateTime(System.currentTimeMillis() - TimeUnit.SECONDS.toMillis(30)).toString("yyyy-MM-dd'T'HH:mm:ss.SSSZZ"))
             .getMillis()));
     }
 }
