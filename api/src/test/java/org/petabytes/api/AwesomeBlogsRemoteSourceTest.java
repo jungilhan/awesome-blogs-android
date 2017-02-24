60fa742e4dcf06e753ee2d6b8aebafa269a9506c
Jungil Han <jungil.han@gmail.com>
2017-02-25 01:50:35 +0800

    #9 테스트 코드(AwesomeBlogsRemoteSourceTest) 빌드 에러 수정

--- api/src/test/java/org/petabytes/api/AwesomeBlogsRemoteSourceTest.java
+++ api/src/test/java/org/petabytes/api/AwesomeBlogsRemoteSourceTest.java
@@ -1,41 +1,30 @@
 package org.petabytes.api;
 
 import org.junit.Test;
-import org.petabytes.api.source.local.Feed;
 import org.petabytes.api.source.remote.AwesomeBlogsRemoteSource;
-
-import rx.functions.Action1;
 
 public class AwesomeBlogsRemoteSourceTest {
 
     @Test
-    public void feeds() throws Exception {
-        new AwesomeBlogsRemoteSource(new Action1<Feed>() {
-            @Override
-            public void call(Feed feed) {}
-        }, true).getFeed("dev")
-                .test()
-                .assertCompleted();
+    public void feed() throws Exception {
+        new AwesomeBlogsRemoteSource(true)
+            .getFeed("dev")
+            .test()
+            .assertCompleted();
 
-        new AwesomeBlogsRemoteSource(new Action1<Feed>() {
-            @Override
-            public void call(Feed feed) {}
-        }, true).getFeed("company")
-                .test()
-                .assertCompleted();
+        new AwesomeBlogsRemoteSource(true)
+            .getFeed("company")
+            .test()
+            .assertCompleted();
 
-        new AwesomeBlogsRemoteSource(new Action1<Feed>() {
-            @Override
-            public void call(Feed feed) {}
-        }, true).getFeed("insightful")
-                .test()
-                .assertCompleted();
+        new AwesomeBlogsRemoteSource(true)
+            .getFeed("insightful")
+            .test()
+            .assertCompleted();
 
-        new AwesomeBlogsRemoteSource(new Action1<Feed>() {
-            @Override
-            public void call(Feed feed) {}
-        }, true).getFeed("all")
-                .test()
-                .assertCompleted();
+        new AwesomeBlogsRemoteSource(true)
+            .getFeed("all")
+            .test()
+            .assertCompleted();
     }
 }
