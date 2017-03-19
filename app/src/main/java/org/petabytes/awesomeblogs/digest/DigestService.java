aecae811f2db5f6d51fe547d1f5c3a89d67cd507
Jungil Han <jungil.han@gmail.com>
2017-03-19 16:19:18 +0900

    #34 다이제스트 알림, 제목을 최대 세 개까지 보여주도록 처리

--- app/src/main/java/org/petabytes/awesomeblogs/digest/DigestService.java
+++ app/src/main/java/org/petabytes/awesomeblogs/digest/DigestService.java
@@ -2,13 +2,16 @@ package org.petabytes.awesomeblogs.digest;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import org.petabytes.api.source.local.Entry;
 import org.petabytes.awesomeblogs.AwesomeBlogsApp;
 import org.petabytes.awesomeblogs.R;
 import org.petabytes.awesomeblogs.fcm.Notifications;
 import org.petabytes.awesomeblogs.util.Analytics;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import hugo.weaving.DebugLog;
@@ -34,12 +37,34 @@ public class DigestService extends IntentService {
             .observeOn(AndroidSchedulers.mainThread())
             .doOnNext($ -> Analytics.event(Analytics.Event.SEND_DIGEST))
             .subscribe(entries ->
-                Notifications.send(this, getString(R.string.digest_title, entries.size()), entries.size() == 1
-                    ? getString(R.string.fresh_entries_title_0, entries.get(0).getTitle())
-                    : getString(R.string.fresh_entries_title_1, entries.get(0).getTitle(), (entries.size() - 1))));
+                Notifications.send(this, getString(R.string.digest_title, entries.size()), createMessage(entries)));
 
         AwesomeBlogsApp.get().api().getFeed("all", false)
             .onErrorResumeNext(Observable.empty())
             .subscribe();
+    }
+
+    private String createMessage(@NonNull List<Entry> entries) {
+        String message;
+        switch (entries.size()) {
+            case 0:
+                throw new IllegalStateException("Size cannot be zero.");
+            case 1:
+                message = getString(R.string.digest_message_1, entries.get(0).getTitle());
+                break;
+            case 2:
+                message = getString(R.string.digest_message_2,
+                    entries.get(0).getTitle(), entries.get(1).getTitle());
+                break;
+            case 3:
+                message = getString(R.string.digest_message_3,
+                    entries.get(0).getTitle(), entries.get(1).getTitle(), entries.get(2).getTitle());
+                break;
+            default:
+                message = getString(R.string.digest_message_4,
+                    entries.get(0).getTitle(), entries.get(1).getTitle(), entries.get(2).getTitle(), entries.size() - 3);
+                break;
+        }
+        return message;
     }
 }
