60c4b750510f5d20f018593489e2c692b49808db
Jungil Han <jungil.han@gmail.com>
2017-06-03 11:59:26 +0100

    앱 실행 시, 현재 카테고리 외 다른 카테고리도 일괄적으로 업데이트 하도록 처리

--- app/src/main/java/org/petabytes/awesomeblogs/feeds/FeedsCoordinator.java
+++ app/src/main/java/org/petabytes/awesomeblogs/feeds/FeedsCoordinator.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.androidadvance.topsnackbar.TSnackbar;
+import com.annimon.stream.Stream;
 import com.squareup.coordinators.Coordinators;
 
 import org.petabytes.api.source.local.Entry;
@@ -31,17 +32,21 @@ import org.petabytes.coordinator.PagerFactory;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import butterknife.BindView;
 import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 import fr.castorflex.android.verticalviewpager.VerticalViewPager;
 import hugo.weaving.DebugLog;
+import rx.Observable;
 import rx.functions.Action3;
 import rx.schedulers.Schedulers;
 
+import static org.petabytes.awesomeblogs.R.id.company;
 import static org.petabytes.awesomeblogs.feeds.FeedsCoordinator.Type.CIRCLE;
 import static org.petabytes.awesomeblogs.feeds.FeedsCoordinator.Type.DIAGONAL;
 import static org.petabytes.awesomeblogs.feeds.FeedsCoordinator.Type.ENTIRE;
@@ -121,7 +126,22 @@ class FeedsCoordinator extends Coordinator {
             .filter($ -> TextUtils.equals(this.category, category))
             .map(Feed::getEntries)
             .map(this::categorize)
-            .subscribeOn(Schedulers.io()), this::onLoad, $ -> onLoadError());
+            .subscribeOn(Schedulers.io())
+            .doOnNext($ -> {
+                if (refresh) {
+                    return;
+                }
+                Set<String> categories = new HashSet<String>() {{
+                    add("all"); add("dev"); add("company"); add("insightful");
+                }};
+                categories.remove(category);
+                Stream.of(categories).forEach(c ->
+                    AwesomeBlogsApp.get().api()
+                        .getFeed(c, true)
+                        .subscribeOn(Schedulers.io())
+                        .onErrorResumeNext(Observable.empty())
+                        .subscribe());
+            }), this::onLoad, $ -> onLoadError());
     }
 
     private void onLoad(@NonNull List<Map<Type, List<Entry>>> entries) {
