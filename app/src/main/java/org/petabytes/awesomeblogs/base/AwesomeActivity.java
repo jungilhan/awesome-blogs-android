ed3ddc3cfdb7d806503c64b49b6418fdaf8bdcf0
Jungil Han <jungil.han@gmail.com>
2017-12-02 15:56:34 +0900

    검색, 즐겨찾기, 최근 읽은 목록에 대한 앱 바로가기 기능 추가(>= api 25)

--- app/src/main/java/org/petabytes/awesomeblogs/base/AwesomeActivity.java
+++ app/src/main/java/org/petabytes/awesomeblogs/base/AwesomeActivity.java
@@ -1,7 +1,10 @@
 package org.petabytes.awesomeblogs.base;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NavUtils;
+import android.support.v4.app.TaskStackBuilder;
 
 import org.petabytes.awesomeblogs.AwesomeBlogsApp;
 import org.petabytes.coordinator.Activity;
@@ -19,5 +22,17 @@ public abstract class AwesomeActivity extends Activity {
     @Override
     protected ActivityLayoutBinder createActivityLayoutBinder() {
         return AwesomeBlogsApp.get().activityLayoutBinder();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent parentActivityIntent = NavUtils.getParentActivityIntent(this);
+        if (isTaskRoot() && parentActivityIntent != null) {
+            TaskStackBuilder.create(this)
+                .addNextIntent(parentActivityIntent)
+                .startActivities();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
